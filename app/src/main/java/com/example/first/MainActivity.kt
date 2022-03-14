6c4bfe3f72ab7e65e88ad500a50e1c3d7fc97a2a
Balpreet Singh <balpreet8151@gmail.com>
2022-03-14 11:40:43 +0530

    initial commit

---
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.first
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("start" , "i see a button" )
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+            Log.d("click", " the button is clicked")
+            val intent = Intent(this , secondActivity::class.java)
+            startActivity(intent)
+            Log.d("new Activity " , " second Activity is opened")
+        }
+
+
+
+
+
+
+    }
+}
